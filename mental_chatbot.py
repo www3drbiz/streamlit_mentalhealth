62514a22d49de3d879642b2d5171b14eb7d3fd26
www3drbiz <www3drbiz@gmail.com>
2024-12-05 09:56:30 +0900

    commit

---
+++ mental_chatbot.py
@@ -0,0 +1,51 @@
+import streamlit as st
+from streamlit_chat import message
+import pandas as pd
+from sentence_transformers import SentenceTransformer
+from sklearn.metrics.pairwise import cosine_similarity
+import json
+
+# Set page configuration
+st.set_page_config(page_title="상담봇", page_icon="🤖")
+
+@st.cache_resource()
+def cached_model():
+    model = SentenceTransformer('jhgan/ko-sroberta-multitask')
+    return model
+
+@st.cache_data()
+def get_dataset():
+    df = pd.read_csv('wellness_dataset.csv')
+    df['embedding'] = df['embedding'].apply(json.loads)
+    return df
+
+model = cached_model()
+df = get_dataset()
+
+#st.header('심리상담 챗봇')
+#st.markdown("[❤️빵형의 개발도상국](https://www.youtube.com/c/빵형의개발도상국)")
+st.title("🤖 상담 봇")
+if 'generated' not in st.session_state:
+    st.session_state['generated'] = []
+
+if 'past' not in st.session_state:
+    st.session_state['past'] = []
+
+for i in range(len(st.session_state['past'])):
+    message(st.session_state['past'][i], is_user=True, key=str(i) + '_user')
+    if len(st.session_state['generated']) > i:
+        message(st.session_state['generated'][i], key=str(i) + '_bot')
+
+with st.form('form', clear_on_submit=True):
+    user_input = st.text_input('당신: ', '')
+    submitted = st.form_submit_button('전송')
+
+if submitted and user_input:
+    embedding = model.encode(user_input)
+
+    df['distance'] = df['embedding'].map(lambda x: cosine_similarity([embedding], [x]).squeeze())
+    answer = df.loc[df['distance'].idxmax()]
+
+    st.session_state.past.append(user_input)
+    st.session_state.generated.append(answer['챗봇'])
+
